2e8d926a03bfe6f225b210ea20fcc4a8924f9eb7
wnncry <bronipower03@gmial.com>
2024-04-09 01:53:23 +0300

    First commit

---
+++ main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    context = {
+        'title': 'Home',
+        'content': 'Main page'
+    }
+
+
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    return HttpResponse('About page')
