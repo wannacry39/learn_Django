af74793dc79b427a6ff384cb505bdee4e9965bd0
wnncry <bronipower03@gmial.com>
2024-04-15 02:31:27 +0300

    started making goods

--- first_django_proj/urls.py
+++ first_django_proj/urls.py
@@ -20,5 +20,8 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace='main'))
-]
+    path('', include('main.urls', namespace='main')),
+    path('catalog/', include('goods.urls', namespace='catalog'))
+    ]
+    
+    
