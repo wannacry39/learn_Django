58f1abc9207cf423ce6decce8937e761256bd10d
wnncry <bronipower03@gmial.com>
2024-04-21 21:00:49 +0300

    Add simple tag for categories

--- goods/views.py
+++ goods/views.py
@@ -1,18 +1,16 @@
 from django.shortcuts import render
-from goods.models import Categories, Products
+from goods.models import Products
 
 # Create your views here.
 
 
 def catalog(request):
 
-    categories = Categories.objects.all()
     goods = Products.objects.all()
 
     context = {
         "title": "Catalog",
         "goods": goods,
-        "categories": categories
     }
     return render(request, 'goods/catalog.html', context)
 
