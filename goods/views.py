054e6878aaf836fb9885bff726d11105d00d2b7f
wnncry <bronipower03@gmial.com>
2024-04-21 23:49:54 +0300

    done urls

--- goods/views.py
+++ goods/views.py
@@ -4,9 +4,13 @@ from goods.models import Products
 # Create your views here.
 
 
-def catalog(request):
+def catalog(request, category_slug):
+    
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = Products.objects.filter(category__slug=category_slug)
 
-    goods = Products.objects.all()
 
     context = {
         "title": "Catalog",
